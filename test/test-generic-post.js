a6f850bf4caf5ae4b436962fc29013f4447a09d6
gulfalaska <153951625+gulfalaska@users.noreply.github.com>
2025-04-18 10:31:50 -0400

    Update test-generic-post.js

--- test/test-generic-post.js
+++ test/test-generic-post.js
@@ -61,14 +61,6 @@ describe("check build output for a generic post", () => {
       expect(css).to.not.match(/test-dead-code-elimination-sentinel/);
     });
 
-    it("should have script elements", () => {
-      const scripts = doc.querySelectorAll("script[src]");
-      let has_ga_id = GA_ID ? 1 : 0;
-      expect(scripts).to.have.length(has_ga_id + 1); // NOTE: update this when adding more <script>
-      expect(scripts[0].getAttribute("src")).to.match(
-        /^\/js\/min\.js\?hash=\w+/
-      );
-    });
 
     it("should have GA a setup", () => {
       if (!GA_ID) {
