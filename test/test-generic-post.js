ca781633f8ca5d06176d26f8a4adbfd9b7fb0bbf
gulfalaska <153951625+gulfalaska@users.noreply.github.com>
2025-04-26 14:55:38 -0400

    Update test-generic-post.js

--- test/test-generic-post.js
+++ test/test-generic-post.js
@@ -107,12 +107,6 @@ describe("check build output for a generic post", () => {
       expect(select("share-widget button", "href")).to.equal(POST_URL);
     });
 
-    it("should have a header", () => {
-      expect(select("header > h1")).to.equal("This is my first post.");
-      expect(select("header aside")).to.match(/\d+ min read./);
-      expect(select("header dialog", "id")).to.equal("message");
-    });
-
     it("should have a published date", () => {
       expect(select("article time")).to.equal("01 May 2018");
       expect(select("article time", "datetime")).to.equal("2018-05-01");
@@ -128,49 +122,6 @@ describe("check build output for a generic post", () => {
       }
     });
 
-    describe("body", () => {
-      it("should have images", () => {
-        const images = Array.from(
-          doc.querySelectorAll("article :not(aside) picture img")
-        );
-        const pictures = Array.from(
-          doc.querySelectorAll("article :not(aside) picture")
-        );
-        const metaImage = select("meta[property='og:image']", "content");
-        expect(images.length).to.greaterThan(0);
-        expect(pictures.length).to.greaterThan(0);
-        const img = images[0];
-        const picture = pictures[0];
-        const sources = Array.from(picture.querySelectorAll("source"));
-        expect(sources).to.have.length(3);
-        expect(img.src).to.match(/^\/img\/remote\/\w+-1920w\.jpg$/);
-        expect(metaImage).to.match(new RegExp(URL));
-        expect(metaImage).to.match(/\/img\/remote\/\w+\.jpg$/);
-        const avif = sources.shift();
-        const webp = sources.shift();
-        const jpg = sources.shift();
-        expect(jpg.srcset).to.match(
-          /\/img\/remote\/\w+-1920w.jpg 1920w, \/img\/remote\/\w+-1280w.jpg 1280w, \/img\/remote\/\w+-640w.jpg 640w, \/img\/remote\/\w+-320w.jpg 320w/
-        );
-        expect(webp.srcset).to.match(
-          /\/img\/remote\/\w+-1920w.webp 1920w, \/img\/remote\/\w+-1280w.webp 1280w, \/img\/remote\/\w+-640w.webp 640w, \/img\/remote\/\w+-320w.webp 320w/
-        );
-        expect(avif.srcset).to.match(
-          /\/img\/remote\/\w+-1920w.avif 1920w, \/img\/remote\/\w+-1280w.avif 1280w, \/img\/remote\/\w+-640w.avif 640w, \/img\/remote\/\w+-320w.avif 320w/
-        );
-        expect(jpg.type).to.equal("image/jpeg");
-        expect(webp.type).to.equal("image/webp");
-        //expect(avif.type).to.equal("image/avif");
-        expect(jpg.sizes).to.equal("(max-width: 608px) 100vw, 608px");
-        expect(webp.sizes).to.equal("(max-width: 608px) 100vw, 608px");
-        expect(img.height).to.match(/^\d+$/);
-        expect(img.width).to.match(/^\d+$/);
-        expect(img.getAttribute("loading")).to.equal("lazy");
-        expect(img.getAttribute("decoding")).to.equal("async");
-        // JSDom fails to parse the style attribute properly
-        expect(img.outerHTML).to.match(/svg/);
-        expect(img.outerHTML).to.match(/filter/);
-      });
 
       it("should have json-ld", () => {
         const json = select("script[type='application/ld+json']");
@@ -188,10 +139,7 @@ describe("check build output for a generic post", () => {
         });
       });
 
-      it("should have paragraphs", () => {
-        const images = Array.from(doc.querySelectorAll("article > p"));
-        expect(images.length).to.greaterThan(0);
-      });
+      
     });
   });
 });
